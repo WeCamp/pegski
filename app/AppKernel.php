cf52db90be4cfce56cc39fcccae9585a240aa7e9
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-08-23 22:32:08 +0200

    Removed AppBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Overblog\GraphQLBundle\OverblogGraphQLBundle(),
-            new AppBundle\AppBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {

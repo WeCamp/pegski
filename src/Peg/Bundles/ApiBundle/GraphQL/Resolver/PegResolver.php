714cf92a199b72d0d0f611ce664567a625a7ff6b
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2016-08-24 10:40:25 +0200

    Basic setup for Graphql with fake pegs

---
+++ src/Peg/Bundles/ApiBundle/GraphQL/Resolver/PegResolver.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Peg\Bundles\ApiBundle\GraphQL\Resolver;
+
+class PegResolver
+{
+    public function resolveType($data)
+    {
+        return [
+            [
+                'id' => 'a1dd9640-f371-47ff-b134-0082b70ffb82',
+                'shortcode' => 'a1dd9640'
+            ],
+            [
+                'id' => '610ca2a0-7d67-4a45-bf64-4eaf80d48902',
+                'shortcode' => '610ca2a0'
+            ],
+            [
+                'id' => 'ab5e8cfe-4ffa-41af-91e4-db09108611af',
+                'shortcode' => 'ab5e8cfe'
+            ],
+        ];
+    }
+}

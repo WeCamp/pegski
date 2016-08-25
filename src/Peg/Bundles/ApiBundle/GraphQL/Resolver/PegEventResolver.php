81ebae1d5de7025978382a2844566b56a2acc750
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-08-26 00:08:00 +0200

    Returning actual events

--- src/Peg/Bundles/ApiBundle/GraphQL/Resolver/PegEventResolver.php
+++ src/Peg/Bundles/ApiBundle/GraphQL/Resolver/PegEventResolver.php
@@ -2,6 +2,7 @@
 
 namespace Peg\Bundles\ApiBundle\GraphQL\Resolver;
 
+use Peg\Bundles\ApiBundle\Document\Event;
 use Peg\Bundles\ApiBundle\Document\Peg;
 use Peg\Repository\CommentEventRepositoryInterface;
 use Peg\Repository\LocationEventRepositoryInterface;
@@ -28,15 +29,22 @@ class PegEventResolver
     }
 
 
+    /**
+     * @param Peg $peg
+     *
+     * @return Event[]
+     */
     public function resolveEventsByPeg(Peg $peg): array
     {
-        return [
-            [
-                'id' => 'b614f268-6d73-462c-b4fd-708e804f1f91',
-                'action' => 'Some action',
-                'user_id' => '87baac55-3cb4-4d00-af3f-fc466faa899a',
-                'happenedAt' => (new \DateTime('2016-08-24 10:10:10'))->format(DATE_ATOM),
-            ],
-        ];
+        $commentEvents = $this->commentEventRepository->findAllForPeg($peg);
+        $locationEvents = $this->locationEventRepository->findAllForPeg($peg);
+
+        $events = array_merge($commentEvents, $locationEvents);
+
+        usort($events, function(Event $a, Event $b) {
+            return ($a->getHappenedAt() < $b->getHappenedAt());
+        });
+
+        return $events;
     }
 }

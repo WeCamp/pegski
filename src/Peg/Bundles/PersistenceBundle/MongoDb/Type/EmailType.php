c1cab0ca23773f94ef538bc36c8b13a38352bd5e
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-08-25 11:05:56 +0200

    Added EmailType converter

---
+++ src/Peg/Bundles/PersistenceBundle/MongoDb/Type/EmailType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Peg\Bundles\PersistenceBundle\MongoDb\Type;
+
+use Doctrine\ODM\MongoDB\Types\Type;
+use Peg\Domain\ValueObject\Email;
+
+class EmailType extends Type
+{
+    public function convertToDatabaseValue($value)
+    {
+        if (is_null($value)) {
+            return null;
+        }
+
+        if (!$value instanceof Email) {
+            throw new \InvalidArgumentException('$value must be an email address');
+        }
+
+        return (string) $value;
+    }
+
+    public function convertToPHPValue($value)
+    {
+        if (is_null($value)) {
+            return null;
+        }
+
+        return new Email($value);
+    }
+
+
+    public function closureToPHP()
+    {
+        return '$return = new \Peg\Domain\ValueObject\Email($value);';
+    }
+
+
+}

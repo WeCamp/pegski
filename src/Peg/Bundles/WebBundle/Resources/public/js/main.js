bbbb7728be4693ea0921989486a46b399503c30f
Pim Widdershoven <pim@widdershoven.nl>
2016-08-26 09:51:19 +0200

    Add application.js and register peg button

--- src/Peg/Bundles/WebBundle/Resources/public/js/main.js
+++ src/Peg/Bundles/WebBundle/Resources/public/js/main.js
@@ -40,25 +40,9 @@
 
     }
 
-    function graphQLFetch(query, variables, callback) {
-        var oReq = new XMLHttpRequest();
-        oReq.addEventListener("load", callback);
-        oReq.open("POST", "/app_dev.php/graphql/");
-        //oReq.responseType = 'json';
-        oReq.setRequestHeader('content-type', 'application/json');
-        oReq.setRequestHeader('accept', 'application/json');
-
-        oReq.send(JSON.stringify(
-            {
-                query: query,
-                variables: variables
-            }
-        ));
-    }
-
     function fetchPegs() {
         const fetchPegsQuery = 'query Test { pegs { shortcode } }';
-        graphQLFetch(fetchPegsQuery, {}, reqListener);
+        window.graphQLFetch(fetchPegsQuery, {}, reqListener);
     }
 
     fetchPegs();

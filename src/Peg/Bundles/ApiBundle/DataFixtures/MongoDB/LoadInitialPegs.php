88c3e3742cb94fe3fd16f1d020864dd94ca08a62
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-08-26 22:58:01 +0200

    Loading peg fixtures

---
+++ src/Peg/Bundles/ApiBundle/DataFixtures/MongoDB/LoadInitialPegs.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Peg\Bundles\ApiBundle\DataFixtures\MongoDB;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\SharedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Peg\Bundles\ApiBundle\Document\Peg;
+
+class LoadInitialPegs extends AbstractFixture implements SharedFixtureInterface
+{
+    static public $coaches = ['catalyst', 'flexman', 'giant', 'puppetmaster', 'tacticus', 'voltra'];
+
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::$coaches as $coach) {
+            $peg = Peg::register($coach);
+
+            $this->addReference('peg:' . $coach, $peg);
+
+            $manager->persist($peg);
+        }
+
+        $manager->flush();
+    }
+}
